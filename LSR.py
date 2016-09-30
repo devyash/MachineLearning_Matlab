dbf83a9925f8e17120ab83309b684a6b2fa08fde
Devyash Sanghai <andiosdroid@gmail.com>
2016-09-30 13:29:06 -0400

    LMS

--- LSR.py
+++ LSR.py
@@ -5,7 +5,7 @@ mat_data = sio.loadmat('training.mat')
 #print mat_data.values()
 a= np.array(mat_data['training'])
 print a[1:11]
-print len(a)
+print 
 """
 y = np.random.random(10)
 slope, intercept, r_value, p_value, std_err = stats.linregress(x,y)
