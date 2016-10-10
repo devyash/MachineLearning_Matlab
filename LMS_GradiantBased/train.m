42192ccb8b1973169c60a9593d51d221bf146306
Devyash Sanghai <andiosdroid@gmail.com>
2016-10-10 18:17:19 -0400

    Plotted Learning curve

--- LMS_GradiantBased/train.m
+++ LMS_GradiantBased/train.m
@@ -1,11 +1,11 @@
-function [W,E] = train(W,e,X,Mu,training,M)
+function [W,OUTPUT] = train(W,e,X,Mu,training,M)
     N=size(training,1);
-    E=zeros(N-M,1);
+    OUTPUT=zeros(N-M,1);
     for i=1:N-M
         %N_f=(X(:,i)'*X(:,i));
         N_f=1;
         e=Calculate_E( W,i,training,M,X);
-        E(i)=Calculate_E( W,i,training,M,X);
+        OUTPUT(i,1)=(e)^2;
         W=Calculate_W(W,Mu,e,X,i,M,N_f);
     end
     %figure
