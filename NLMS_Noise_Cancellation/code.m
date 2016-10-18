4b89e7faa4a220069014827e9d90056f8d93db8c
Devyash Sanghai <devyashsanghai@ufl.edu>
2016-10-18 18:53:21 -0400

    Add files via upload

--- NLMS_Noise_Cancellation/code.m
+++ NLMS_Noise_Cancellation/code.m
@@ -1,8 +1,9 @@
 clear;
 load('project1.mat');
 mu=0.1;
-M=2;
+M=14;
 [e,w,W]=nlms1(mu,M,primary,reference);
 plot(1:70000,e);
 title('Error or the desired signal');
 sound(e,fs);
+plot(1:70000,W1(1,:),1:70000,W1(1,:))
--- NLMS_Noise_Cancellation/nlms1.m
+++ NLMS_Noise_Cancellation/nlms1.m
@@ -1,4 +1,4 @@
-function [e,w]=nlms1(mu,M,primary,reference)
+function [e,w,W]=nlms1(mu,M,primary,reference)
 w=zeros(M,1);
 N=length(primary);
 
@@ -7,15 +7,15 @@ primary=primary(:);
 reference=reference(:);
 
 
-%W=zeros(2,N-M);
-%W_I=zeros(1,N-M);
+W=zeros(2,N-M);
+
 %NLMS
 for n=M:N
-uvec=primary(n:-1:n-M+1);
-%W(1,n)= w(1,1);
-%W(2,n)= w(2,1);
-%W_I(3,n)=w(1,1)+w(2,1);
-e(n)=reference(n)-w'*uvec;
-w=w+mu/(uvec'*uvec)*uvec*conj(e(n));
+x=primary(n:-1:n-M+1);
+W(1,n)= w(1,1);
+W(2,n)= w(2,1);
+
+e(n)=reference(n)-w'*x;
+w=w+mu/(x'*x)*x*conj(e(n));
 end
  
