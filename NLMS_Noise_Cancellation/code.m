f4aeb1d19a6e632bd90676d58207d3a42b29a6fa
Devyash Sanghai <devyashsanghai@ufl.edu>
2016-10-18 00:09:52 -0400

    Add files via upload

--- NLMS_Noise_Cancellation/code.m
+++ NLMS_Noise_Cancellation/code.m
@@ -1,8 +1,8 @@
 clear;
 load('project1.mat');
 mu=0.1;
-M=14;
-[e,w]=nlms1(mu,M,primary,reference);
+M=2;
+[e,w,W]=nlms1(mu,M,primary,reference);
 plot(1:70000,e);
 title('Error or the desired signal');
 sound(e,fs);
--- NLMS_Noise_Cancellation/nlms1.m
+++ NLMS_Noise_Cancellation/nlms1.m
@@ -2,16 +2,20 @@ function [e,w]=nlms1(mu,M,primary,reference)
 w=zeros(M,1);
 N=length(primary);
 
-%make sure that primary and reference are colon vectors
+%make sure that primary and reference are column vectors
 primary=primary(:);
 reference=reference(:);
 
-%W=zeros(M,N-M);
+
+%W=zeros(2,N-M);
+%W_I=zeros(1,N-M);
 %NLMS
 for n=M:N
 uvec=primary(n:-1:n-M+1);
+%W(1,n)= w(1,1);
+%W(2,n)= w(2,1);
+%W_I(3,n)=w(1,1)+w(2,1);
 e(n)=reference(n)-w'*uvec;
-%W(:,n-M+1)=w;
 w=w+mu/(uvec'*uvec)*uvec*conj(e(n));
 end
-
+ 
